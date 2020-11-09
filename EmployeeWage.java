256404d9b5fa409dff53e6c65561d41baeb438fc
Rajas1312 <itsmerajas2@gmail.com>
2020-11-09 17:26:13 +0530

    calculatig daily wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -20,10 +20,19 @@ public class EmployeeWage {
 			}
 		
 	}
+	
+	public static void fullDaily() {
+		
+		int wage_perhour=20;
+		int full_time_hr=8;
+		int daily_wage=wage_perhour*full_time_hr;
+		System.out.println("the full time daily wage is  " +daily_wage);
+	}
 
 	public static void main(String[] args) {
 		greet();
 		attandance();
+		fullDaily();
 	}
 
 }
