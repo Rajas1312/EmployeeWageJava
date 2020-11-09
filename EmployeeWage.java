64f75d1dc4fbbd4b09bd0aaa725564f529c89e85
Rajas1312 <itsmerajas2@gmail.com>
2020-11-09 16:33:41 +0530

    Greeting in Program

---
+++ EmployeeWage.java
@@ -0,0 +1,15 @@
+package employewage;
+
+public class EmployeeWage {
+	
+	public static void greet() {
+		
+		System.out.println("Welcome to Employee wage computation programe");
+	}
+
+	public static void main(String[] args) {
+		greet();
+
+	}
+
+}
